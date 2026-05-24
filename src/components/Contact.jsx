0fbe2a5e9ea8e2197f003e618d829d50c81fbeb6
Rakib Hosain <rakibhosain2302@gmail.com>
2026-05-24 22:37:28 +0600

    My Portfolio tablet contact and fqa  section responsive

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -29,7 +29,7 @@ const Contact = () => {
         </Row>
 
         <Row className="contact-row g-4">
-          <div className="col-md-6 contact-info text-start">
+          <div className="col-md-12 col-lg-4 col-12 contact-info text-start">
             <div className="cont-title">
               <strong>Need More Info?</strong>
               <h4>I'd Love to hear From You!</h4>
@@ -64,7 +64,7 @@ const Contact = () => {
               </div>
             </div>
           </div>
-          <Col md={6}>
+          <div className="col-md-12 col-lg-4 col-12 contact-form-col">
             <Form className="contact-form">
               <Form.Group className="mb-3">
                 <Form.Control type="text" placeholder="Your Name" />
@@ -93,7 +93,7 @@ const Contact = () => {
 
               </button>
             </Form>
-          </Col>
+          </div>
         </Row>
       </div>
     </section>
